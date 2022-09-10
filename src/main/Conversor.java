30012823b252da402857426c6ffc11c79783ee08
mauro137 <maurolizarriaga2@gmail.com>
2022-09-10 20:52:13 -0300

    se redondeo y configura a 2 decimales, ahora se muestra la moneda convertida al usuario

--- src/main/Conversor.java
+++ src/main/Conversor.java
@@ -1,11 +1,8 @@
 package main;
-
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Set;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
 import javax.swing.JOptionPane;
 
 public class Conversor {
@@ -37,12 +34,15 @@ public class Conversor {
 		String[] key = map.keySet().toArray(new String[0]);
 		System.out.println(Arrays.toString(key));
 
+		// seleccionar moneda, si cancela se cierra el programa
 		String money = (String) JOptionPane.showInputDialog(null, "Seleccione su moneda", "Menu",
 				JOptionPane.DEFAULT_OPTION, null, key, key[0]);
 		if (money == null) {
 			main.Util.endMessage();
 			System.exit(0);
 		}
+
+		// ingresar monto, no permite espacios, nulos o caracteres especiales
 		double firstMoney = map.get(money);
 		do {
 			try {
@@ -63,18 +63,20 @@ public class Conversor {
 
 		} while (repeat);
 
+		// si cancela o devuelve nulo se cierra el programa
 		String money2 = (String) JOptionPane.showInputDialog(null, "A que moneda desea convertir?", "Menu",
 				JOptionPane.DEFAULT_OPTION, null, key, key[0]);
 		if (money2 == null) {
 			main.Util.endMessage();
 			System.exit(0);
 		}
+
 		double secondMoney = map.get(money2);
+		// formula para realizar la conversion + mensaje de devolucion
 		double resultado = (monto * firstMoney) * secondMoney;
-		JOptionPane.showMessageDialog(null, "Usted tiene $" + resultado, "System Message", JOptionPane.WARNING_MESSAGE);
-		// hay que redondear el resultado con big decimal
-		// hay que conseguir get clave del segundo input para imprimir
-
+		//se setea 2 decimales y redondea parejo
+		BigDecimal formatear = new BigDecimal(resultado);
+		JOptionPane.showMessageDialog(null, "Usted tiene $" + formatear.setScale(2, RoundingMode.HALF_EVEN) + " " +money2,
+				"System Message", JOptionPane.WARNING_MESSAGE);
 	}
-
 }
